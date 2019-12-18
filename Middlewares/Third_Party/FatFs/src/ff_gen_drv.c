6aa0d754516690ff8b66d00f75e27382d548143d
Merve Berik <merve-berik@yandex.com>
2019-12-18 18:02:30 +0300

    for ignore

--- Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
+++ Middlewares/Third_Party/FatFs/src/ff_gen_drv.c
@@ -1,22 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    ff_gen_drv.c
-  * @author  MCD Application Team
-  * @brief   FatFs generic low level driver.
-  *****************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "ff_gen_drv.h"
 
